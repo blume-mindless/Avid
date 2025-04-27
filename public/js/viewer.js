a4ab38b479d2594e6d5dc665ba4801a5b6dfabcb
mindless <ayoubamriog101@gmail.com>
2025-04-27 22:29:27 +0100

    Updated video uploader

--- public/js/viewer.js
+++ public/js/viewer.js
@@ -1,13 +1,12 @@
-// Configuration (same as upload.js)
 const AZURE_STORAGE_ACCOUNT = "blume";
-const SAS_TOKEN = "&sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-07-01T05:02:43Z&st=2025-04-27T21:02:43Z&spr=https&sig=7as6U8fEHynntgF1yHlNPAuZKTXlQpiKqeohnO02QnM%3D";
+const SAS_TOKEN = "sv=2024-11-04&ss=bfqt&srt=sco&sp=rwdlacupiytfx&se=2025-07-01T05:02:43Z&st=2025-04-27T21:02:43Z&spr=https&sig=7as6U8fEHynntgF1yHlNPAuZKTXlQpiKqeohnO02QnM%3D";
 const CONTAINER_NAME = "videos";
 
 async function loadVideos() {
     const videoContainer = document.getElementById('video-container');
     videoContainer.innerHTML = '';
     
-    const listUrl = `https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}?restype=container&comp=list${SAS_TOKEN}`;
+    const listUrl = `https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}?restype=container&comp=list&${SAS_TOKEN}`;
     
     try {
         const response = await fetch(listUrl);
@@ -18,7 +17,7 @@ async function loadVideos() {
         
         for (let blob of blobs) {
             const blobName = blob.getElementsByTagName("Name")[0].textContent;
-            const videoUrl = `https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}/${blobName}${SAS_TOKEN}`;
+            const videoUrl = `https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${CONTAINER_NAME}/${blobName}?${SAS_TOKEN}`;
             
             const videoElement = document.createElement('div');
             videoElement.className = 'video-item';
@@ -38,5 +37,4 @@ async function loadVideos() {
     }
 }
 
-// Load videos when page loads
 document.addEventListener('DOMContentLoaded', loadVideos);
